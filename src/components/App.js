2e0109413028e3999fd9193235447f77863506ac
Bejoy Mathew <bejoygm@gmail.com>
2017-05-28 10:47:29 +0530

    added lifecycle hooks

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,26 @@ import React from 'react';
 
 import Header from './Header';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header message="Naming Contests" />
-      <div>
-        ...
+class App extends React.Component {
+  state = { 
+    pageHeader: 'Naming Contests' 
+  };
+  componentDidMount() {
+   
+  }
+  componentWillUnmount() {
+    
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header message="Naming Contests" />
+        <div>
+          {this.state.pageHeader}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
